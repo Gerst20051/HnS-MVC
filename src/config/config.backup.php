538f373545885963494cb6419ea306a41ef5465f
Andrew Gerst <gerst20051@gmail.com>
2013-04-26 18:26:26 -0400

    updated to git files

--- src/config/config.backup.php
+++ src/config/config.backup.php
@@ -2,10 +2,10 @@
 
 define('DEVELOPMENT_ENVIRONMENT',false);
 
-define('DB_NAME','hns');
-define('DB_USER','root');
+define('DB_NAME','');
+define('DB_USER','');
 define('DB_PASSWORD','');
-define('DB_HOST','localhost');
+define('DB_HOST','');
 
 define('SITE_NAME','Social HnS');
 define('BASE_PATH','http://'.$_SERVER['HTTP_HOST'].'/mvc');
